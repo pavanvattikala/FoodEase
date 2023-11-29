56a009d85d4aaa342d27daf2f70b4d62eb896310
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-11-29 19:43:41 +0530

    serve with local ip

---
+++ app/Console/Commands/ServeWithLocalIP.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ServeWithLocalIP extends Command
+{
+    protected $signature = 'serve:local';
+    protected $description = 'Run the Laravel development server with a static local IP';
+
+    public function handle()
+    {
+        $localIP = '192.168.29.57';
+
+        $this->info("Running 'php artisan serve' with local Wi-Fi IP: $localIP");
+
+        // Run the artisan serve command
+        exec("php artisan serve --host=$localIP --port=80");
+    }
+}
