1f56dfe4ded2f94f3a89a6bd94a068c7349e7af8
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-11-29 22:05:20 +0530

    main routes

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,30 @@ use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\Frontend\CategoryController as FrontendCategoryController;
 use App\Http\Controllers\Frontend\MenuController as FrontendMenuController;
 use App\Http\Controllers\Frontend\ReservationController as FrontendReservationController;
+use App\Http\Controllers\Frontend\OrdersController as FrontendOrdersController;
+use App\Http\Controllers\WaiterController;
 use App\Http\Controllers\Frontend\WelcomeController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
-
+use PHPUnit\TextUI\XmlConfiguration\Group;
 
 Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/categories', [FrontendCategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/{category}', [FrontendCategoryController::class, 'show'])->name('categories.show');
 Route::get('/menus', [FrontendMenuController::class, 'index'])->name('menus.index');
+
+// Route::middleware(['auth','waiter'])->group(function () {
+//     // display menu
+//     Route::get('/orders/step-one', [FrontendOrdersController::class, 'stepone'])->name('orders.step.one');    
+// });
+
+Route::get('/orders/step-one', [FrontendOrdersController::class, 'stepone'])->name('orders.step.one');    
+
+Route::get('/waiter/request-waiter', [WaiterController::class, 'requestWaiter'])->name('request.waiter.new');
+Route::get('/waiter/request-bill', [WaiterController::class, 'requestWaiter'])->name('request.waiter.bill');
+Route::get('/waiter/request-extra', [WaiterController::class, 'requestWaiter'])->name('request.waiter.extra');
+
+
 Route::get('/reservation/step-one', [FrontendReservationController::class, 'stepOne'])->name('reservations.step.one');
 Route::post('/reservation/step-one', [FrontendReservationController::class, 'storeStepOne'])->name('reservations.store.step.one');
 Route::get('/reservation/step-two', [FrontendReservationController::class, 'stepTwo'])->name('reservations.step.two');
