5c84635596d24f94693dd479620fee3344a7dd58
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-11-29 22:02:31 +0530

    waiter controller, middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\Admin;
+use App\Http\Middleware\WaiterMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -64,6 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => Admin::class
+        'admin' => Admin::class,
+        'waiter' => WaiterMiddleware::class,
     ];
 }
---
+++ app/Http/Middleware/WaiterMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\EmployeeCategory;
+use SebastianBergmann\Environment\Console;
+
+class WaiterMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Check if the authenticated user is a waiter
+        $waiterCategory = EmployeeCategory::where('name',"waiter")->first()->id;
+
+        if ($request->user() && $request->user()->category_id == $waiterCategory) {
+            return $next($request);
+        }
+
+        // Redirect or respond as needed for non-waiter users
+        abort(403, 'Unauthorized access.'); // You can customize the response as needed
+    }
+}
