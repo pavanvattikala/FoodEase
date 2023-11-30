d30ff0ecfb5c75d844989b6b69d5eaa17e25a98e
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-11-30 13:52:05 +0530

    requests

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    //
+
+    public function requestWaiter(Request $request){
+        //return waiter
+    }
+
+    public function requestBill(Request $request){
+        //return bill
+    }
+
+    public function requestExtra(Request $request){
+        //return extra
+    }
+}
