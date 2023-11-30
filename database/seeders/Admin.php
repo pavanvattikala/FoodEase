b5ec1d940097501f308688ee060a0688bce3e36b
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-11-30 13:53:58 +0530

    admin seed

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EmployeeCategory;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,13 +17,19 @@ class Admin extends Seeder
      */
     public function run()
     {
+
+        EmployeeCategory::create([
+            'name' => 'admin'
+        ]);
+
+
         User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'is_admin' => 1
+            'category_id' => 1
         ]);
     }
 }
