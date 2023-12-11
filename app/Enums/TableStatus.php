aa987471874925ecec9e8a1e807ac0cc099db310
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-11 12:36:32 +0530

    Fix typo in TableStatus enum

--- app/Enums/TableStatus.php
+++ app/Enums/TableStatus.php
@@ -5,6 +5,6 @@ namespace App\Enums;
 enum TableStatus: string
 {
     case Pending = 'pending';
-    case Avalaiable = 'avaliable';
+    case Available = 'available';
     case Unavaliable = 'unavaliable';
 }
