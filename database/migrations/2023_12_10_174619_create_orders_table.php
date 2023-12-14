efc761b27b4cc15b76e12bb19e78a42b2d9af27b
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-14 21:11:41 +0530

    Add OrderStatus and OrderType enums to create_orders_table migration

--- database/migrations/2023_12_10_174619_create_orders_table.php
+++ database/migrations/2023_12_10_174619_create_orders_table.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
 return new class extends Migration
 {
     /**
@@ -13,19 +14,22 @@ return new class extends Migration
      */
     public function up()
     {
+
+
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('KOT');
             $table->decimal('total', 8, 2);
-            $table->unsignedBigInteger('tableNo');
-            $table->foreign('tableNo')->references('id')->on('tables')->onDelete('cascade');
-            $table->enum('status', ['new', 'processing', 'ready_for_pickup', 'served', 'closed'])->default('new');
+            $table->unsignedBigInteger('table_id');
+            $table->foreign('table_id')->references('id')->on('tables')->onDelete('cascade');
+            $table->string('status')->default(OrderStatus::New->value);
             $table->text('special_instructions')->nullable();
-            $table->enum('order_type', ['dine_in', 'takeaway'])->default('dine_in');
+            $table->string('order_type')->default(OrderType::DineIn->value);
             $table->unsignedBigInteger('waiter_id');
             $table->foreign('waiter_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
+
     }
 
     /**
