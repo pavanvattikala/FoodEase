08df2d776c944c360f906ec0d0d1cce5045f27ec
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-14 21:13:31 +0530

    Add fillable and casts properties to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'KOT',
+        'total',
+        'tableNo',
+        'status',
+        'special_instructions',
+        'order_type',
+        'waiter_id',
+    ];
+
+    protected $casts = [
+        'status' => OrderStatus::class,
+        'order_type' => OrderType::class,
+    ];
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class, 'tableNo');
+    }
+
+    public function waiter()
+    {
+        return $this->belongsTo(User::class, 'waiter_id');
+    }
 }
