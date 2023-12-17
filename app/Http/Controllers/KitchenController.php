39d82a3ca02e906680f9896d0d930497b17d5aa4
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-17 20:51:34 +0530

    Refactor KitchenController and kitchen.blade.php

--- app/Http/Controllers/KitchenController.php
+++ app/Http/Controllers/KitchenController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\OrderStatus;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class KitchenController extends Controller
 {
     //
     public function index(){
-        return view("kitchen.index");
+        $orders = Order::with('orderDetails.menu') // Adjust the relationship names based on your actual structure
+            ->with('waiter')
+            ->where('status',OrderStatus::New->value)
+            ->orderBy('created_at')
+            ->get();
+
+        
+        return view("kitchen.index",compact('orders'));
     }
 }
