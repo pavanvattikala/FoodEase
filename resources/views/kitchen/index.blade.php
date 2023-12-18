8999371c223f4f9bd0c93a282cf8f90ec737ef0f
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-18 21:05:04 +0530

    Refactor kitchen index.blade.php layout

--- resources/views/kitchen/index.blade.php
+++ resources/views/kitchen/index.blade.php
@@ -12,34 +12,37 @@
         </div>
         <div class="container max-w-max px-2 processing-orders">
             <h1 class="text-2xl font-semibold w-full align-top text-center">Current Orders</h1>
-    
-            @if($processingOrders->isEmpty())
-                <p class="text-gray-500 w-full">No order available.</p>
-            @else
-                <div class="flex flex-wrap">
-                    @foreach($processingOrders as $order)
-                        <x-order-processing-component-for-kitchen :order="$order" />
-                    @endforeach
-                </div>
-            @endif
+            <div class="flex flex-wrap">
+                @if($processingOrders->isEmpty())
+                    <p class="text-gray-500 w-full">No order available.</p>
+                @else
+                
+                        @foreach($processingOrders as $order)
+                            <x-order-processing-component-for-kitchen :order="$order" />
+                        @endforeach
+                    
+                @endif
+            </div>
         </div>
-        {{-- <div class="container max-w-xs items-counts">
-            <h1 class="text-2xl font-semibold w-full mb-4">Orders Count</h1>
-            @if($orders->isEmpty())
-                <p class="text-gray-500 w-full">No order available.</p>
-            @else
-                    <div class="max-w-max bg-slate-400 border border-gray-500 rounded pt-4 m-2 font-bold">
-                        <div class="border-t border-gray-500 pt-2">
-                            <p class="mb-1">{{ $orderDetail->quantity }} x {{ $orderDetail->menu->name }} </p>
-                        </div>
+        <div class="items-counts" style="min-width: 15rem;">
+            <h1 class="text-2xl font-semibold w-full align-top text-center">Items Count</h1>
+            <div class="flex flex-col pt-4">
 
-                        <div class="border-t border-gray-500 pt-2">
-                            <p class="mb-1">{{ $orderDetail->quantity }} x {{ $orderDetail->menu->name }} </p>
-                        </div>
-  
+                <div class="dine-in max-w-max bg-slate-400 border border-gray-500 rounded pt-4 m-2 font-bold">
+                    <h1 class="text-2xl font-semibold">Dine in Orders</h1>
+                    <div class="border-t border-gray-500 p-2 items">
+                        <p class=" mb-1 order-name-quantity"> </p>
                     </div>
-            @endif
-        </div> --}}
+                </div>
+                <div class="take-away max-w-max bg-slate-400 border border-gray-500 rounded pt-4 m-2 font-bold">
+                    <h1 class="text-2xl font-semibold" >Takeaway Orders</h1>
+                    <div class="border-t border-gray-500 p-2 items">
+                        <p class="mb-1 order-name-quantity"> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
    
     <script>
@@ -67,6 +70,8 @@
                         });
                     }, 5000); // 5 seconds
                 });
+
+                calculateMenuSumByType();
         });
 
         function acceptOrder(orderId){
@@ -80,12 +85,17 @@
                 data:{orderId:orderId},
                 contentType:'application/x-www-form-urlencoded',
                 success: function (response) {
-                    var order = $('#order'+orderId).clone();
-                    order.find('.options').remove();
+                    var order = $('.pending-orders > #order'+orderId).clone();
+                    order.find('.options').children().last().remove();
+                    order.find('.options').children().first().attr("id","completeOrder");
+                    order.find('.options').children().first().attr("onclick","completeOrder("+orderId+")");
                     $('.processing-orders > .flex').append(order);
-                    $('#order' + orderId).remove();
-
+                    $('.pending-orders > #order' + orderId).remove();
                     console.log('Order accepted');
+                    
+                    calculateMenuSumByType();
+            
+                    
                 },
                 error: function (error) {
                     console.error('Error ', error);
@@ -106,8 +116,10 @@
                 contentType:'application/x-www-form-urlencoded',
                 success: function (response) {
                     $('#order' + orderId).remove();
+                    calculateMenuSumByType();
 
                     console.log('Order discarded');
+
                 },
                 error: function (error) {
                     console.error('Error ', error);
@@ -128,6 +140,7 @@
                 success: function (response) {
                     $('#order' + orderId).remove();
                     console.log('Order No '+orderId+' marked as completed');
+                    calculateMenuSumByType();
 
                 },
                 error: function (error) {
@@ -135,6 +148,73 @@
                 }
             });
         }
+
+
+    // Function to iterate through orders and calculate the sum of menu items based on dine-in or takeaway
+    function calculateMenuSumByType() {
+        console.log('calculateMenuSumByType');
+    // Get all elements with class "order-item"
+    const orderElements = document.querySelectorAll('.processing-orders  .order-item');
+
+    // Create objects to store the sum of each menu item for dine-in and takeaway
+    const dineInSum = {};
+    const takeawaySum = {};
+
+    // Iterate through each order element
+    orderElements.forEach((orderElement) => {
+        // Get quantity elements
+        const quantityElements = orderElement.querySelectorAll('.order-details .order-name-quantity');
+
+        // Get the order type description
+        const orderTypeDescription = orderElement.querySelector('.order-type').textContent.trim();
+
+        // Determine if it's dine-in or takeaway based on order type description
+        const isDineIn = orderTypeDescription.toLowerCase() === 'dine in';
+        
+
+        quantityElements.forEach((quantityElement) => {
+        // Extract menu item and quantity
+        const [quantity, menuItem] = quantityElement.textContent.split(' x ');
+
+        // Update the sum for the menu item based on dine-in or takeaway
+        const menuItemKey = menuItem.trim();
+        const quantityValue = parseInt(quantity.trim(), 10);
+
+        if (isDineIn) {
+            dineInSum[menuItemKey] = (dineInSum[menuItemKey] || 0) + quantityValue;
+        } else {
+            takeawaySum[menuItemKey] = (takeawaySum[menuItemKey] || 0) + quantityValue;
+        }
+        });
+
+    });
+
+    $('.dine-in > .items').empty();
+    $('.take-away > .items').empty();
+
+    // Output the results
+    //console.log('Dine-In Menu Item Sums:');
+    Object.entries(dineInSum).forEach(([menuItem, sum]) => {
+        //console.log(`${menuItem}: ${sum}`);
+        updateMenuSumByType('dinein',menuItem, sum);
+    });
+
+   // console.log('\nTakeaway Menu Item Sums:');
+    Object.entries(takeawaySum).forEach(([menuItem, sum]) => {
+       // console.log(`${menuItem}: ${sum}`);
+       updateMenuSumByType('takeaway',menuItem, sum);
+    });
+    }
+
+    function updateMenuSumByType( type, menuItem, sum) {
+        //console.log('updateMenuSumByType');
+        if(type==='dinein')
+            $('.dine-in > .items').append('<p class="mb-1 order-name-quantity">'+sum+' x '+menuItem+'</p>');
+        else{
+            $('.take-away > .items').append('<p class="mb-1 order-name-quantity">'+sum+' x '+menuItem+'</p>');
+        }
+    }
+
 
     </script>
 </x-kitchen-layout>
