60c381614bb6be7167c482d042ee7ba373e2786e
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-18 21:04:55 +0530

    Refactor order component markup

--- resources/views/components/new-order-component-for-kitchen.blade.php
+++ resources/views/components/new-order-component-for-kitchen.blade.php
@@ -1,12 +1,12 @@
-<div class="max-w-xs bg-slate-400 border border-gray-500 rounded pt-4 m-2 font-bold" id="order{{$order->id}}">
+<div class="max-w-xs bg-slate-400 border border-gray-500 rounded pt-4 m-2 font-bold order-item" id="order{{$order->id}}">
     <div class="flex">
         <div class="w-1/4">
-            <p class="mb-2 px-4">{{ $order->created_at->format('g:i a') }}</p>
+            <p class="mb-2 px-4 order-time">{{ $order->created_at->format('g:i a') }}</p>
             <p class="mb-2 px-4">Table: {{ $order->table_id }}</p>
         </div>
         <div class="w-3/4">
-            <p class="mb-2 px-4">{{ $order->waiter->name }}</p>
-            <p class="mb-2 px-4">{{ \App\Enums\OrderType::getDescription($order->order_type) }}</p>                  
+            <p class="mb-2 px-4 waiter-name">{{ $order->waiter->name }}</p>
+            <p class="mb-2 px-4 order-type">{{ \App\Enums\OrderType::getDescription($order->order_type) }}</p>                  
         </div>                        
     </div>
     <div>
@@ -18,10 +18,10 @@
     <div class="mt-4 px-4 order-details">
         @foreach($order->orderDetails as $orderDetail)
             <div class="border-t border-gray-500 pt-2">
-                <p class="mb-1">{{ $orderDetail->quantity }} x {{ $orderDetail->menu->name }} </p>
+                <p class="mb-1 order-name-quantity">{{ $orderDetail->quantity }} x {{ $orderDetail->menu->name }} </p>
             </div>
         @endforeach
-    </div>     
+    </div>
     <div class="flex justify-center options">
         <button class="m-2 p-2 rounded" id="acceptOrder" onclick="acceptOrder({{ $order->id }})">✅</button>
         <button class="m-2 p-2 rounded" id="discardOrder" onclick="discardOrder({{ $order->id }})">❌</button>
