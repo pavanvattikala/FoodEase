256ca0f4193b41ac0c0ff8bc7f2006be8bb0fbfb
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-25 10:32:10 +0530

    Add flatpickr CSS and JS files

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -3,8 +3,10 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/flatpickr.css') }}">
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ mix('js/datepicker.js')}}"></script>
