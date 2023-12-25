c193f427c465e723a83be2a20a9418bdeb65f09a
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-25 09:30:23 +0530

    Add RestaurantServiceProvider binding and remove RestaurantServiceProvider class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind(RestaurantServiceProvider::class);
+
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\RestaurantServiceProvider::class,
 
 
         /*
