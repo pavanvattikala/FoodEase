5b8db78e8d494d86f7959703ed7713aa8b8076bf
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-01-03 23:04:42 +0530

    Add nullable constraint to table_id column in orders table

--- database/migrations/2023_12_10_174619_create_orders_table.php
+++ database/migrations/2023_12_10_174619_create_orders_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Enums\OrderStatus;
 use App\Enums\OrderType;
+
 return new class extends Migration
 {
     /**
@@ -20,7 +21,7 @@ return new class extends Migration
             $table->id();
             $table->string('KOT');
             $table->decimal('total', 8, 2);
-            $table->unsignedBigInteger('table_id');
+            $table->unsignedBigInteger('table_id')->nullable();
             $table->foreign('table_id')->references('id')->on('tables')->onDelete('cascade');
             $table->string('status')->default(OrderStatus::New->value);
             $table->text('special_instructions')->nullable();
@@ -29,7 +30,6 @@ return new class extends Migration
             $table->foreign('waiter_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
-
     }
 
     /**
