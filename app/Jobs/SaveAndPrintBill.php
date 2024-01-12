95c4e5b7c6334d11d6147ea9a5b69e5afad4c4dc
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-01-12 20:12:22 +0530

    Add SaveAndPrintBill job and update queue configuration
    Add migration for jobs table

---
+++ app/Jobs/SaveAndPrintBill.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Helpers\PDFHelper;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class SaveAndPrintBill implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $billId;
+    private $billPath;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($billId)
+    {
+        $this->billId = $billId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $this->billPath =  PDFHelper::saveBillToDisk($this->billId);
+        PDFHelper::printBill($this->billPath);
+    }
+}
