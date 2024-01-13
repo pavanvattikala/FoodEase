59bae543f13e85fac7dcaa7226eeda593598d4bf
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-01-13 23:29:30 +0530

    Add new table status options

--- app/Enums/TableStatus.php
+++ app/Enums/TableStatus.php
@@ -5,5 +5,9 @@ namespace App\Enums;
 enum TableStatus: string
 {
     case Available = 'available';
+    case Running = 'running';
+    case RunningKOT = 'runningKOT';
+    case Printed = 'printed';
+    case Paid = 'paid';
     case Unavaliable = 'unavaliable';
 }
