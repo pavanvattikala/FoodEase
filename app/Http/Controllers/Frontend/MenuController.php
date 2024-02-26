9b029166878eb1e332a7e77b76e052e73971b9ee
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-02-26 14:56:43 +0530

    psr-4 bug

--- app/Http/Controllers/Frontend/MenuController.php
+++ app/Http/Controllers/Frontend/MenuController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Frontend;
+namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
 use App\Models\Menu;
--- app/Http/Controllers/Frontend/ReservationController.php
+++ app/Http/Controllers/Frontend/ReservationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Frontend;
+namespace App\Http\Controllers\FrontEnd;
 
 use App\Enums\TableStatus;
 use App\Http\Controllers\Controller;
@@ -18,7 +18,7 @@ class ReservationController extends Controller
     public function stepZero(Request $request)
     {
         $menus = Menu::all();
-        return view('reservations.step-zero',compact('menus'));
+        return view('reservations.step-zero', compact('menus'));
     }
 
     public function stepOne(Request $request)
@@ -58,7 +58,7 @@ class ReservationController extends Controller
         $res_table_ids = Reservation::orderBy('res_date')->get()->filter(function ($value) use ($reservation) {
             return $value->res_date->format('Y-m-d') == $reservation->res_date->format('Y-m-d');
         })->pluck('table_id');
-        $tables = Table::where('status', TableStatus::Avalaiable)
+        $tables = Table::where('status', TableStatus::Available)
             ->where('guest_number', '>=', $reservation->guest_number)
             ->whereNotIn('id', $res_table_ids)->get();
         return view('reservations.step-two', compact('reservation', 'tables'));
