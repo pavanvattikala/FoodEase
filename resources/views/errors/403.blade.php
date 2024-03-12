34b82a119297373e325442c494dafb33eaf446f1
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-03-12 15:06:10 +0530

    Update 403 error

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,16 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403 Forbidden</title>
+</head>
+
+<body>
+    <h1>403 Forbidden</h1>
+    <p>Access to this resource is forbidden.</p>
+    <a href="{{ route('dashboard') }}">Go to Home</a>
+</body>
+
+</html>
