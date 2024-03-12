50e0d69ecacb0d1f41f52732fb1b5258279210f4
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-03-12 12:25:21 +0530

    Add pin field to users table and update UserSeeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('pin')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('category_id');
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -32,6 +32,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
+            'pin' => '1',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -40,6 +41,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Waiter',
             'email' => 'waiter@gmail.com',
+            'pin' => '2',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -48,10 +50,11 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Kitchen',
             'email' => 'kitchen@gmail.com',
+            'pin' => '3',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'category_id' => 2
+            'category_id' => 3
         ]);
     }
 }
