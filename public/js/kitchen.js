36a1128e1285c81eabfe1249fcc735fb0699b47d
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-08-12 17:57:32 +0530

    ui update

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -6372,8 +6372,8 @@ __webpack_require__.r(__webpack_exports__);
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: "pusher",
-  key: "24926435665875e208a7",
-  cluster: "ap2",
+  key: "",
+  cluster: "mt1",
   forceTLS: true
 });
 })();
