c9f8b4c5e8739225c01491f1834b324f4b5098a4
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2024-08-26 09:34:46 +0530

    modify billling

--- app/Http/Controllers/Billing/BillController.php
+++ app/Http/Controllers/Billing/BillController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Billing;
 
 use App\Enums\UserRole;
 use App\Helpers\PDFHelper;
+use App\Http\Controllers\Controller;
 use App\Models\Bill;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
